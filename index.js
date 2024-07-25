3b908933a3090a671c4be383b5fb0c9b0be11a0d
eustachekamala <eustachejared25@gmail.com>
2024-07-25 16:30:20 +0300

    I added 3 class to return something like sex and name of animal and return their language(kkk)

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Your code here
+class Cat{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak(){
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak(){
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else{
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
+let cat = new Cat("max","male")
+
+cat.speak();
+
+let dog = new Dog("Rox", "Female")
+
+dog.speak();
+
+let bird = new Bird("Rex","male")
+let bird2 = new Bird("Mable","male")
+
+bird.speak()
+bird2.speak()
